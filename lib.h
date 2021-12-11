1ca56831685de000991125a5c59ec7fdab6dfa8d
Marebu <91844335+Marebu@users.noreply.github.com>
2021-12-11 16:17:46 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 #ifndef UNTITLED15_LIB_H
 #define UNTITLED15_LIB_H
 
-float NumeroMassimo(const float numeri[], int dimensione);
+float numeroMassimo(const float numeri[], int dimensione);
 
 #endif //UNTITLED15_LIB_H
