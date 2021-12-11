7d5c9506478a295d68b49a26e20e79963e9739be
Marebu <91844335+Marebu@users.noreply.github.com>
2021-12-11 15:58:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+  int dim;
+  cin >> dim;
+  float numeri [dim];
+  for (int i=0 ; i<dim ; i++){
+    cin >> numeri[i];
+  }
+  NumeroMassimo(numeri, dim);
+  
+  cout << NumeroMassimo(numeri, dim) << endl;
   return 0;
 }
