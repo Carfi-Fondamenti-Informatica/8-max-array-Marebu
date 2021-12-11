948a8f04335b36bc96d4100b39e412c782139dba
Marebu <91844335+Marebu@users.noreply.github.com>
2021-12-11 17:01:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,11 +1,10 @@
-
 #include "lib.h"
 float numeroMassimo( const float numeri[], int dim){
-  numeri [dim];
+  numeri [dim] ;
   float massimo = numeri[0];
   for (int i=0 ; i < dim ; i++){
     if(massimo < numeri[i]){
-      massimo = numeri[1];
+      massimo = numeri[i];
     }
   }
   
